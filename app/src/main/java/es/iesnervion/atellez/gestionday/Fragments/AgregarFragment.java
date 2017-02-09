25351eed49ef7a4bbdf110ca5225a08a440533d0
alvarotellez <alvarotellezhidalgo@gmail.com>
2017-02-09 13:09:22 +0100

    Rellenar Spinner

--- app/src/main/java/es/iesnervion/atellez/gestionday/Fragments/AgregarFragment.java
+++ app/src/main/java/es/iesnervion/atellez/gestionday/Fragments/AgregarFragment.java
@@ -21,6 +21,14 @@ public class AgregarFragment extends Fragment {
     {
 
         //miSpinner = (Spinner) getActivity().findViewById(R.id.spinnerTipo);
+        //Spinner spinner = (Spinner) getActivity().findViewById(R.id.spinnerTipo);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+       // ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(),
+                //R.array.planets_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        //spinner.setAdapter(adapter);
         return inflater.inflate(R.layout.fragment_agregar, container, false);
 
     }
