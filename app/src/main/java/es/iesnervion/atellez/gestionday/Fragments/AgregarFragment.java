c6db2a24693d5bbc19c09f06824a89c47b405d2e
alvarotellez <alvaro.tellez.hidalgo@gmail.com>
2017-02-09 01:13:30 +0100

    Pasito a pasito...

--- app/src/main/java/es/iesnervion/atellez/gestionday/Fragments/AgregarFragment.java
+++ app/src/main/java/es/iesnervion/atellez/gestionday/Fragments/AgregarFragment.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 import es.iesnervion.atellez.gestionday.R;
 
@@ -18,7 +16,7 @@ public class AgregarFragment extends Fragment{
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-            return inflater.inflate(R.layout.fragment_agregar, container, false);
+        return inflater.inflate(R.layout.fragment_agregar, container, false);
 
     }
 
